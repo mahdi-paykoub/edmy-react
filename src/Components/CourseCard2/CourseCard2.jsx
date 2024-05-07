639c7e79221789b6cc7c679b99fb541aa22da790
mahdi paykoub <mahdi.paykoub.github@gmail.com>
2024-05-07 12:16:49 -0800

    add remove cart items from cart page

--- src/Components/CourseCard2/CourseCard2.jsx
+++ src/Components/CourseCard2/CourseCard2.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './style.css'
-import Col from 'react-bootstrap/Col';
 import Toman from "../Toman/Toman";
 import { BiTimer } from 'react-icons/bi';
+import { CartContext } from "../../Context/CartContext";
 
 
-export default function CourseCard2({ name, image, slug, price }) {
+export default function CourseCard2({ name, image, slug, price, id }) {
     const baseUrl = process.env.REACT_APP_BASE_URL
+    const cartContext = useContext(CartContext)
+
 
     return (
         <>
             <div
                 className='p-4 bg-white br5 box-course-cart d-block d-md-flex justify-content-md-between'>
                 <div className='d-block d-md-flex text-center text-md-end'>
-                    <img src={baseUrl+image} className='mw-100 h-auto br5' alt="" />
+                    <img src={baseUrl + image} className='mw-100 h-auto br5' alt="" />
                     <div className='me-3'>
                         <div className='fw800 mt-4 mt-md-0'>
                             {name}
@@ -35,7 +37,9 @@ export default function CourseCard2({ name, image, slug, price }) {
                         <Toman w={18} h={18} className='me-1 porple-text-color2' />
                     </div>
                     <div>
-                        <button className='btn porple-text-color2 p-0 mt-md-3 me-4 me-md-0 fs15 hover-red'>حذف</button>
+                        <button className='btn porple-text-color2 p-0 mt-md-3 me-4 me-md-0 fs15 hover-red'
+                            onClick={() => cartContext.removeFromCart(id)}
+                        >حذف</button>
                     </div>
                 </div>
             </div>
